95a6f39c1a8dcbd83c011329cba791d37e571901
noel-jose <noeljosetp@gmail.com>
2022-02-11 17:50:23 +0530

    update the signal

--- tasks/signals.py
+++ tasks/signals.py
@@ -4,12 +4,28 @@ from django.dispatch import receiver
 
 
 @receiver(pre_save, sender=Task)
-def create_taskhistory(sender, instance, **kwargs):
-    if Task.objects.filter(id=instance.id).exists():
-        prev_status = Task.objects.filter(id=instance.id).values_list("status")
+def create_taskhistory(sender, instance, update_fields, **kwargs):
+    print("The update fields are ", update_fields)
+    # if Task.objects.filter(id=instance.id).exists():
+    #     prev_status = Task.objects.filter(id=instance.id).values_list("status")
+    #     curr_status = instance.status
+    #     if prev_status != curr_status:
+    #         history = TaskHistory(
+    #             previous_status=prev_status, current_status=curr_status, task=instance
+    #         )
+    #         history.save()
+    #         print(f"Successfully created a history for the task {instance.title}")
+
+    if instance.id is None:
+        pass
+    else:
+        prev_status = Task.objects.get(id=instance.id).status
         curr_status = instance.status
-        history = TaskHistory(
-            previous_status=prev_status, current_status=curr_status, task=instance
-        )
-        history.save()
-        print(f"Successfully created a history for the task {instance.title}")
+        if curr_status != prev_status:
+            history = TaskHistory(
+                previous_status=prev_status, current_status=curr_status, task=instance
+            )
+            history.save()
+            print(
+                f"Successfully created a new history for the change {prev_status} --> {curr_status}"
+            )
